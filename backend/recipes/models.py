a1c035debf69f19bf4f3a4dbdcaa07d897af0806
Frank Stotch <ya.inside38@gmail.com>
2024-08-14 23:35:50 +0500

    add user model and its validator

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .validators import validate_username
 
-# Create your models here.
+
+MAX_LENGTH_EMAIL = 254
+MAX_LENGTH_NAME = 256
+MAX_LENGTH_SLUG = 50
+MAX_LENGTH_USERNAME = 150
+
+
+class Role(models.TextChoices):
+    user = ('user', 'Пользователь')
+    admin = ('admin', 'Администратор')
+
+
+class HelpText:
+    NAME = f'Не более {MAX_LENGTH_NAME} символов'
+    SLUG = (
+        f'Не более {MAX_LENGTH_SLUG} символов. Обязан быть уникальным'
+    )
+    USERNAME = (
+        f'Максимум {MAX_LENGTH_USERNAME} символов. Допускаются '
+        'буквы, цифры и символы @/./+/- .'
+    )
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=MAX_LENGTH_USERNAME,
+        help_text=HelpText.USERNAME,
+        unique=True,
+        validators=[
+            validate_username,
+        ],
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=max(len(choice) for choice in list(Role)),
+        choices=Role.choices,
+        default=Role.user,
+    )
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        max_length=MAX_LENGTH_EMAIL,
+        unique=True
+    )
+
+    @property
+    def is_admin(self):
+        return self.role == Role.admin or self.is_staff
+
+    class Meta:
+        ordering = ('username',)
