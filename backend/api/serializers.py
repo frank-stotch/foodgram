d8c239c947d23459569eeaf28f329f3b73341608
Frank Stotch <ya.inside38@gmail.com>
2024-08-23 15:23:32 +0500

    add preparations for separating serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -80,108 +80,12 @@ class IngredientRecipeSerializer(serializers.ModelSerializer):
         read_only_fields = fields
 
 
-class RecipeSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True)
-    author = UserSerializer(many=True)
-    is_favorited = serializers.SerializerMethodField()
-    is_in_shopping_cart = serializers.SerializerMethodField()
-    ingredients = IngredientRecipeSerializer(many=True)
-
-    class Meta:
-        model = Recipe
-        fields = (
-            "id",
-            "tags",
-            "author",
-            "ingredients",
-            "is_favorited",
-            "is_in_shopping_cart",
-            "name",
-            "image",
-            "text",
-            "cooking_time",
-        )
-        read_only_fields = (
-            "id",
-            "author",
-            "is_favorited",
-            "is_in_shopping_cart",
-        )
-
-    @transaction.atomic
-    def create(self, validated_data):
-        tags = validated_data.pop("tags")
-        if not tags:
-            raise serializers.ValidationError(REQUIRED_FIELD_MISSING)
-        ingredients_data = validated_data.pop("ingredients")
-        if not ingredients_data:
-            raise serializers.ValidationError(REQUIRED_FIELD_MISSING)
-        recipe = Recipe.objects.create(**validated_data)
-        TagRecipe.objects.bulk_create(
-            [TagRecipe(recipe=recipe, tag=tag) for tag in tags]
-        )
-        IngredientRecipe.objects.bulk_create(
-            [
-                IngredientRecipe(
-                    recipe=recipe,
-                    ingredient=ingredient["id"],
-                    amount=ingredient["amount"],
-                )
-                for ingredient in ingredients_data
-            ]
-        )
-        return recipe
-
-    @transaction.atomic
-    def update(self, recipe, validated_data):
-        ingredients_data = validated_data.pop("ingredients")
-        if not ingredients_data:
-            raise serializers.ValidationError(REQUIRED_FIELD_MISSING)
-        tags = validated_data.pop("tags")
-        if not tags:
-            raise serializers.ValidationError(REQUIRED_FIELD_MISSING)
-        IngredientRecipe.objects.filter(recipe=recipe).exclude(
-            id__in=[i["id"] for i in ingredients_data]
-        ).delete()
-        TagRecipe.objects.filter(recipe=recipe).exclude(
-            tag_id__in=[t["id"] for t in tags]
-        ).delete()
-        IngredientRecipe.objects.bulk_create(
-            [
-                IngredientRecipe(
-                    recipe=recipe,
-                    ingredient=ingredient["id"],
-                    amount=ingredient["amount"],
-                )
-                for ingredient in ingredients_data
-            ],
-            ignore_conflicts=True,
-        )
-        TagRecipe.objects.bulk_create(
-            [TagRecipe(recipe=recipe, tag=tag) for tag in tags],
-            ignore_conflicts=True,
-        )
-        return super().update(recipe, validated_data)
-
-    def get_is_favorited(self, recipe):
-        user = self.context.get("request").user
-        return (
-            user.is_authenticated
-            and user.favorites.filter(recipe=recipe).exists()
-        )
-
-    def get_is_in_shopping_cart(self, recipe):
-        user = self.context.get("request").user
-        return (
-            user.is_authenticated
-            and user.shoppingcarts.filter(recipe=recipe).exists()
-        )
-
-
 class ReadRecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)
     author = UserSerializer(many=True)
     ingredients = IngredientRecipeSerializer(many=True)
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
 
     class Meta:
         model = Recipe
