9bec1e244a38ed220651986682f95b236d11c144
Frank Stotch <ya.inside38@gmail.com>
2024-09-09 00:19:24 +0500

    del unnecessary get_object_or_404

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.urls import reverse
-from .models import Recipe
 
 
 def short_link_view(request, pk):
-    get_object_or_404(Recipe, pk=pk)
     return redirect(reverse("api:recipes-detail", kwargs={"pk": pk}))
