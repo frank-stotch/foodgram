864e79f79dc61d87e7aa253fabd18f5c38cce77e
Frank Stotch <ya.inside38@gmail.com>
2024-09-11 21:50:21 +0500

    del redundant django.urls.reverse

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import redirect
-from django.urls import reverse
 
 
 def short_link_view(request, pk):
-    return redirect(reverse("api:recipes-detail", kwargs={"pk": pk}))
+    return redirect("api:recipes-detail", kwargs={"pk": pk})
