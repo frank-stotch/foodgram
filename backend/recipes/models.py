e8b5641b44e8564a9431e9c15ef8dcaf462acb07
Frank Stotch <ya.inside38@gmail.com>
2024-09-11 21:54:07 +0500

    add app name to get_absolute_url in Recipe model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -226,7 +226,7 @@ class Recipe(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("short_link", args=[str(self.pk)])
+        return reverse("recipes:short_link", args=[str(self.pk)])
 
 
 class RecipeIngredient(models.Model):
