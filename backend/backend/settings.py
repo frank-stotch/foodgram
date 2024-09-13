d4c43e8acdff2b8ab487b3c86840ab94e3a85d22
Frank Stotch <ya.inside38@gmail.com>
2024-09-13 20:53:08 +0500

    change settings for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -143,7 +143,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "collected_static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = MEDIA_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
